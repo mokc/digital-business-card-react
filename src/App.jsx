fdbfb8fa6a722091e28ffb01351236bdf71318dd
Cheryl <cheryl@Cheryls-Macbook.local>
2026-05-12 00:29:12 -0700

    Add Interests component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Info from './components/Info'
 import About from './components/About'
+import Interests from './components/Interests'
 
 function App() {
   return (
     <>
       <Info />
       <About />
+      <Interests />
     </>
   )
 }
---
+++ src/components/Interests.jsx
@@ -0,0 +1,12 @@
+function Interests() {
+    return (
+        <section>
+            <h2>Interests</h2>
+            <p>
+                Food expert. Climber. Reader. Coffee and matcha fanatic. Dog mom.
+            </p>
+        </section>
+    )
+}
+
+export default Interests
